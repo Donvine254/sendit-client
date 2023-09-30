f241e6dad4fa79d0264eb81ffa0a7ead05d13413
Important Person <126117678+Donvine254@users.noreply.github.com>
2023-10-01 02:24:41 +0300

    Create loading.js

---
+++ components/loading.js
@@ -0,0 +1,16 @@
+//import the loading module.css file here
+export default function Loading(){
+return (
+<div class='container'>
+  <div class='loader'>
+    <div class='loader--dot'></div>
+    <div class='loader--dot'></div>
+    <div class='loader--dot'></div>
+    <div class='loader--dot'></div>
+    <div class='loader--dot'></div>
+    <div class='loader--dot'></div>
+    <div class='loader--text'></div>
+  </div>
+</div>
+  )
+}
