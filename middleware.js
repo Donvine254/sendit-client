ebea3ce1f2cd98082b0cff16e4c2d497a5f79ec8
donvine mugendi <donvinemugendi@gmail.com>
2023-10-01 05:35:05 +0300

    updated middleware

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ export const config = {
      * - favicon.ico (favicon file)
      */
     // "/((?!api|_next/static|_next/image|favicon.ico|^/$).*)",
-    "/((?!api|_next/static|_next/image|favicon.ico|/).*)",
+   "/dashboard"
   ],
 };
 
