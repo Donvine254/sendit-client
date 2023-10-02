d1b0214f14bc3723028f47666cf5a24b4589d410
donvine mugendi <donvinemugendi@gmail.com>
2023-10-02 23:23:28 +0300

    add google map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,47 +1,57 @@
-'use client'
-import React, {useCallback, useState} from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+"use client";
+import React, { useCallback, useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
 
-import Loading from './loading';
+import Loading from "./loading";
 
 const containerStyle = {
-  width: '100%',
-  height: '600px'
+  width: "100%",
+  height: "600px",
 };
 
 const center = {
   lat: -1.2834,
-  lng: 36.8235
+  lng: 36.8235,
 };
 export default function Map() {
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey:"AIzaSyAIs4ASEwyBvSlqKlqyfRFS9QZ-t2pUgu0",
-  })
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
 
-  const [map, setMap] = useState(null)
+  const [map, setMap] = useState(null);
 
   const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
+    <div className="container">
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={12}
+        zoom={10}
         onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <><Loading/></>
+        onUnmount={onUnmount}></GoogleMap>
+    </div>
+  ) : (
+    <>
+      <Loading />
+    </>
+  );
 }
