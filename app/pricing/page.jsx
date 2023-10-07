c7f26c8f000894504dab363cbc88bed80dea4085
donvine mugendi <donvinemugendi@gmail.com>
2023-10-07 03:05:08 +0300

    update pricing page

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -1,25 +1,82 @@
+import React from "react";
 
-import React from 'react'
-
-import Image from 'next/image'
-import { Button } from '@/components'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
-
   return (
-    <div className='m-2 mx-4'>
-     <div className="card w-96 bg-base-200 shadow-xl mt-4">
-  <figure className="px-10 pt-10">
-    <Image src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="Shoes" className="rounded-xl h-fit w-fit" height={1000} width={667}  />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Shoes!</h2>
-    <p>Own this nice pair of shoes today</p>
-    <div className="card-actions">
-      <Button/>
-    </div>
-  </div>
-</div>
-    </div>
-  )
+    <>
+      <h1 className="font-bold text-center text-2xl my-2">What We Offer</h1>
+      <div className="m-2 mx-2 md:mx-4 card-grid-container lg:mx-20">
+        <div className="card max-w-[400px] w-[350px] xsm:w-full border bg-base-100  shadow-xl mt-4 h-[380px] hover:bg-base-200">
+          <div className="card-body items-center text-center">
+            <h2 className="font-bold text-2xl text-blue-600">Regular</h2>
+            <h1 className="font-bold text-xl align-start">
+              KSH 350/ <span className="text-base ">delivery</span>
+            </h1>
+            <ul className="text-start p-2 text-base">
+              <li>✔️ Same Day Delivery within Nairobi</li>
+              <li>✔️ Item tracking</li>
+              <li>✔️ &lt;30 Minutes Pickup</li>
+              <li>✔️ Non-Perishable Goods</li>
+              <li>✔️ 0-20kgs</li>
+              <li>✔️ Door Delivery</li>
+              <li>✔️ Fresh Guarantee</li>
+            </ul>
+            <div className="card-actions">
+              <Link href="/deliveries" className="btn btn-primary">
+                Send Parcel
+              </Link>
+            </div>
+          </div>
+        </div>
+        
+        <div className="card max-w-[400px] w-[350px] xsm:w-full border shadow-xl mt-4 h-[380px] bg-primary text-white hover:bg-accent">
+          <div className="card-body items-center text-center">
+            <h2 className="font-bold text-2xl text-white">
+              Cargo <span className="badge badge-accent">HOT</span>
+            </h2>
+            <h1 className="font-bold text-xl align-start">
+              KSH 750/ <span className="text-base ">delivery</span>
+            </h1>
+            <ul className="text-start p-2 text-base">
+              <li>✔️ Item tracking</li>
+              <li>✔️ &lt;20 Minutes Pickup</li>
+              <li>✔️ Non-Perishable Goods</li>
+              <li>✔️ Maximum 100kgs per parcel</li>
+              <li>✔️ Promo and Special Discounts</li>
+              <li>✔️ Door Delivery</li>
+            </ul>
+            <div className="card-actions">
+              <Link href="/deliveries" className="btn btn-neutral">
+                Send Parcel
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card max-w-[350px] w-[350px] xsm:w-full border bg-base-100  shadow-xl mt-4 h-[380px] hover:bg-base-200">
+          <div className="card-body items-center text-center">
+            <h2 className="font-bold text-2xl text-blue-600">Same Day</h2>
+            <h1 className="font-bold text-xl align-start">
+              KSH 500/ <span className="text-base ">delivery</span>
+            </h1>
+            <ul className="text-start p-2 text-base">
+              <li>✔️ Same Day Delivery</li>
+              <li>✔️ Item tracking</li>
+              <li>✔️ &lt;10 Minutes Pickup</li>
+              <li>✔️ Perishable and Non-Perishable Goods</li>
+              <li>✔️ 0-30kgs</li>
+              <li>✔️ Door Delivery</li>
+              <li>✔️ Fresh Guarantee</li>
+            </ul>
+            <div className="card-actions">
+              <Link href="/deliveries" className="btn btn-primary">
+                Send Parcel
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/hero.jsx
+++ components/hero.jsx
@@ -9,16 +9,17 @@ const Hero = () => {
         <h1 className="text-3xl md:text-5xl lg:text-6xl font-extrabold">
           Deliver Anything, Anywhere, Anytime!
         </h1>
-        <p className="py-4 text-[27px] font-light lg:py-6 leading-loose">
+        <p className="py-4 text-[27px] font-light lg:py-6 md:leading-loose">
           Discover how sendit empowers small businesses, traders and individuals
           through reliable courier delivery services today
         </p>
-        <div className="flex items-center gap-10">
-          <Link href="/delivery" className="btn btn-primary ">
-            Create your first delivery now!
+        <div className="flex items-center gap-5 lg:gap-10">
+          <Link href="/deliveries" className="btn btn-primary">
+            Send Parcel
           </Link>
-          <Link href="/about" className="btn btn-outline">
+          <Link href="/contact" className="btn btn-outline btn-ghost flex items-center justify-start gap-1">
             Learn More
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" fill="#0056F1"/></svg>
           </Link>
         </div>
       </div>
