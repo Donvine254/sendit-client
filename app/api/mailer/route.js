9404ebc568e4788e0e7ad42de119580b16f68cea
donvine mugendi <donvinemugendi@gmail.com>
2023-10-13 01:32:44 +0300

    updated mailer

--- app/api/mailer/route.js
+++ app/api/mailer/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
-import nodemailer from "nodemailer"
-import {google } from "googleapis"
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
 const OAuth2 = google.auth.OAuth2;
-import Email from '@/emails/Email'
-import { render } from '@react-email/render';
+import Email from "@/emails/Email";
+import { render } from "@react-email/render";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 
 const createTransporter = async () => {
   const oauth2Client = new OAuth2(
@@ -13,7 +15,7 @@ const createTransporter = async () => {
   );
 
   oauth2Client.setCredentials({
-    refresh_token: process.env.NEXT_PUBLIC_OAUTH_REFRESH_TOKEN
+    refresh_token: process.env.NEXT_PUBLIC_OAUTH_REFRESH_TOKEN,
   });
 
   const accessToken = await new Promise((resolve, reject) => {
@@ -34,8 +36,7 @@ const createTransporter = async () => {
       clientId: process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_OAUTH_CLIENT_SECRET,
       refreshToken: process.env.NEXT_PUBLIC_OAUTH_REFRESH_TOKEN,
-      
-    }
+    },
   });
 
   return transporter;
@@ -46,29 +47,33 @@ const sendEmail = async (emailOptions) => {
   await emailTransporter.sendMail(emailOptions);
 };
 
-
 export async function POST(req) {
-    const data = await req.json();
-    
-   if(!data || !data.subject ||!data.message || !data.email){
-    return NextResponse.json({message: "Missing email body and recipient information"})
-   }
-    try {
-      const emailHtml = render(<Email subject={data.subject} body={data.message}/>);
-      await sendEmail({
-            subject: data.subject,
-            to: data.email,
-            from: process.env.NEXT_PUBLIC_EMAIL,
-            html: emailHtml
-          });
-      return NextResponse.json({ message: "Email sent successfully" });
-    } catch (error) {
-      return new Response("email delivery failed", { status: 400 });
-    }
-  }
+  const data = await req.json();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-
-  export async function GET(){
-    const data={message: "did you know that next js can create api's too?"}
-    return NextResponse.json(data);
+  if (!data || !data.subject || !data.message ) {
+    return NextResponse.json({
+      message: "Missing email body and recipient information",
+    });
   }
+  try {
+    const emailHtml = render(
+      <Email subject={data.subject} body={data.message} />
+    );
+    await sendEmail({
+      subject: data.subject,
+      to: user.email,
+      from: process.env.NEXT_PUBLIC_EMAIL,
+      html: emailHtml,
+    });
+    return NextResponse.json({ message: "Email sent successfully" });
+  } catch (error) {
+    return new Response("email delivery failed", { status: 400 });
+  }
+}
+
+export async function GET() {
+  const data = { message: "did you know that next js can create api's too?" };
+  return NextResponse.json(data);
+}
