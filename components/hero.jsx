c721887cea0181e73c85ad04b1375921163c4560
donvine mugendi <donvinemugendi@gmail.com>
2023-10-16 05:18:29 +0300

    add react icons to home page

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,14 +11,15 @@ const Hero = () => {
         </h1>
         <p className="py-4 text-[27px] font-light lg:py-6 md:leading-loose">
           Discover how sendit empowers small businesses, traders and individuals
-          through reliable courier delivery services today.
+          through reliable courier delivery services today
         </p>
         <div className="flex items-center gap-5 lg:gap-10">
           <Link href="/deliveries" className="btn btn-primary">
             Send Parcel
           </Link>
-          <Link href="/contact" className="btn btn-outline btn-ghost flex items-center justify-start gap-1">
-            Learn More &#x27F6;
+          <Link href="/contact" className="btn btn-outline btn-ghost flex items-center justify-start gap-1 hover:btn-info">
+            Learn More
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" fill="#00000"/></svg>
           </Link>
         </div>
       </div>
