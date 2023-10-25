e4042b436a7574a2c8488a661fb9dcea3ab77df6
donvine mugendi <donvinemugendi@gmail.com>
2023-10-26 00:47:11 +0300

    added rider revenue card

--- components/Dashboard/Riderdashboard.jsx
+++ components/Dashboard/Riderdashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import MyDeliveries from "./MyDeliveries"
+import MyDeliveries from "./MyDeliveries";
 import GetOrders from "./GetOrders";
-export default function Riderdashboard({currentUser, active}) {
+import RiderEarnings from "./Riderearnings";
+export default function Riderdashboard({ currentUser, active }) {
   return (
     <>
-    {active ==="My Deliveries" && <MyDeliveries currentUser={currentUser}/>}
-    {active ==="Get Orders" && <GetOrders />}
+      {active === "My Deliveries" && <MyDeliveries currentUser={currentUser} />}
+      {active === "Get Orders" && <GetOrders />}
+      {active === "Earnings" && <RiderEarnings currentUser={currentUser} />}
     </>
-  )
+  );
 }
