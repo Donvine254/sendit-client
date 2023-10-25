2fba91fc587dd7b22ccd9ee09ce1bbd060b5cd56
donvine mugendi <donvinemugendi@gmail.com>
2023-10-25 21:28:02 +0300

    create rider and admindashbaord

---
+++ components/Dashboard/Admindashbaord.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Admindashbaord() {
+  return (
+    <div>Admindashbaord</div>
+  )
+}
---
+++ components/Dashboard/Riderdashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Riderdashboard() {
+  return (
+    <div>Riderdashboard</div>
+  )
+}
