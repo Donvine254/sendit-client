e1eb78ee281dd37634cfae2de487a0d4bc3146ad
donvine mugendi <donvinemugendi@gmail.com>
2023-10-26 12:44:07 +0300

    add whatsapp icon

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-import { Hero, HomeComponent } from '@/components'
-
+import Image from "next/image";
+import { Hero, HomeComponent } from "@/components";
+import { IoLogoWhatsapp } from "react-icons/io";
+import Link from "next/link";
+import "./contact/contact.css";
 export default function Home() {
-  
   return (
-     <>
-    <Hero/>
-    <div className="divider divide-dotted"></div>
-   <HomeComponent/>
+    <>
+      <Hero />
+      <div className="divider divide-dotted"></div>
+      <HomeComponent />
+      <Link href="" className="fixed bottom-5 right-5 z-50 w-[60px] h-[60px] rounded-full bg-green-500 flex items-center justify-center shadow-md whatsapp-btn">
+        {" "}
+        <IoLogoWhatsapp className="i !text-white fill-white text-[32px] icony cursor-pointer" />
+      </Link>
     </>
-  )
+  );
 }
