d4b63fc06a8725b5a8feab33c4bad7aee8de951a
donvine mugendi <donvinemugendi@gmail.com>
2023-10-26 13:02:38 +0300

    update referror policy

--- app/page.js
+++ app/page.js
@@ -10,6 +10,8 @@ export default function Home() {
       <HomeComponent />
       <Link
         href="https://wa.me/message/KTXORQNJ32XNI1"
+        target="_blank"
+        referrerPolicy="no-referrer"
         className="fixed bottom-5 right-5 z-50 w-[60px] h-[60px] rounded-full bg-green-500 flex items-center justify-center shadow-md whatsapp-btn">
         {" "}
         <IoLogoWhatsapp className="i !text-white fill-white text-[32px] icony cursor-pointer" />
