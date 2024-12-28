e2071b04bcad432dab8e1cbedf1442099397155e
Donvine <donvinemugendi@gmail.com>
2024-12-28 22:04:16 +0300

    style: enhance Navbar component with user avatar and dropdown functionality

--- components/ui/nav-items.tsx
+++ components/ui/nav-items.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 type Props = {};
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { ChevronDown } from "lucide-react";
 export default function Navbar({}: Props) {
   const { user } = useKindeBrowserClient();
 
@@ -23,33 +24,50 @@ export default function Navbar({}: Props) {
       <button
         data-collapse-toggle="mobile-menu"
         type="button"
-        className="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
+        className="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none rounded-lg inline-flex items-center justify-center"
         aria-controls="mobile-menu-2"
         aria-expanded="false">
         <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"></path>
-        </svg>
-        <svg
-          className="hidden w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"></path>
-        </svg>
+        {user && user.picture ? (
+          <>
+            <Image
+              src={user.picture}
+              width={32}
+              height={32}
+              alt="avatar"
+              title="open menu"
+              className="w-8 h-8 rounded-full focus:outline-none focus-within:outline-none"
+            />
+            <ChevronDown className="h-5 w-5" />
+          </>
+        ) : (
+          <>
+            {" "}
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"></path>
+            </svg>
+            <svg
+              className="hidden w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"></path>
+            </svg>
+          </>
+        )}
       </button>
-      <div className="hidden md:block w-full md:w-auto" id="mobile-menu">
-        <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-lg md:font-medium">
+      <div className="hidden md:block w-full md:w-auto " id="mobile-menu">
+        <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-lg md:font-medium ">
           <li>
             <Link
               href="/"
@@ -125,13 +143,26 @@ export default function Navbar({}: Props) {
               </div>
             </div>
           </li>
+
           {/* dropdown for account */}
           <li className={`${!user ? "hidden" : "block"}`}>
             <button
               id="dropdownNavbarLink"
               data-dropdown-toggle="dropdownNavbar"
               className=" hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-2  md:p-0 font-medium flex items-center justify-between w-full md:w-auto hover:underline underline-offset-2">
-              My Account{" "}
+              {user && (
+                <Image
+                  src={
+                    user?.picture ||
+                    "https://res.cloudinary.com/dipkbpinx/image/upload/v1734556978/carhub/avatars/paqrtcgyypq1qelyzrwj.png"
+                  }
+                  width={32}
+                  height={32}
+                  alt="avatar"
+                  title="open menu"
+                  className="w-8 h-8 rounded-full focus:outline-none focus-within:outline-none"
+                />
+              )}
               <svg
                 className="w-4 h-4 ml-1"
                 fill="currentColor"
@@ -169,6 +200,11 @@ export default function Navbar({}: Props) {
                     Settings
                   </a>
                 </li>
+                <li>
+                  <LogoutLink className="text-sm hover:bg-destructive hover:text-destructive-foreground text-gray-700 block px-4 py-2">
+                    Logout
+                  </LogoutLink>
+                </li>
               </ul>
             </div>
           </li>
@@ -176,11 +212,6 @@ export default function Navbar({}: Props) {
             <LoginLink className="text-gray-700 hover:bg-gray-50 xsm:border-b  md:hover:bg-blue-600 md:hover:shadow  md:bg-blue-500 block pl-3 pr-4 py-2 md:hover:text-white md:py-0 md:px-4 items-center md:text-white md:text-center md:rounded-md md:h-8">
               Login
             </LoginLink>
-          </li>
-          <li className={`${!user ? "hidden" : "flex"} `}>
-            <LogoutLink className=" text-gray-700 hover:bg-gray-50 xsm:border-b  md:hover:bg-blue-500 md:hover:shadow  md:bg-blue-600  pl-3 pr-4 py-2 md:hover:text-white md:py-0 md:h-8 md:px-4 md:text-white md:text-center md:rounded-md  items-center">
-              Logout
-            </LogoutLink>
           </li>
         </ul>
       </div>
