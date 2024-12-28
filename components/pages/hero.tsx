16d78a584e03350ee5562d470087c9170d67912f
Donvine <donvinemugendi@gmail.com>
2024-12-28 21:31:13 +0300

    style: enhance hero section layout and add star icon for visual appeal

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -1,19 +1,19 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Star } from "lucide-react";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Heropage() {
   return (
-    <section className="min-h-screen bg-[#F8F9FA]">
-      <div className="bg-gradient-to-br from-blue-400 via-gray-100 to-blue-400 relative md:h-screen min-h-[600px] md:bg-white">
+    <section className="h-screen bg-[#F8F9FA]">
+      <div className="bg-gradient-to-br from-blue-400 via-gray-100 to-blue-400 relative h-screen min-h-[600px] md:bg-white">
         <div
           className="absolute inset-0 bg-[url('https://res.cloudinary.com/dipkbpinx/image/upload/t_hiring-banner/v1735326614/illustrations/cstnqioumwvu8iinrq0r.webp')] bg-no-repeat bg-right bg-contain md:block hidden w-full"
           aria-hidden="true"
         />
         <div className="relative max-w-7xl mx-auto px-4 py-20 sm:px-6 lg:px-8">
           <div className="max-w-lg">
-            <h1 className="text-6xl font-extrabold md:font-bold tracking-tight md:text-gray-900  md:text-7xl lg:mt-4 ">
+            <h1 className="text-6xl font-extrabold md:font-bold tracking-tight md:text-gray-900  md:text-7xl lg:mt-4 text-center sm:text-start xsm:my-8 xsm:tracking-normal">
               Fast & Reliable{" "}
               <span className="text-blue-400 block leading-tight">
                 Courier Services
@@ -39,7 +39,7 @@ export default function Heropage() {
                 </Button>
               </Link>
             </div>
-            {/* <div className="flex mt-4 md:mt-2">
+            <div className="flex mt-4 md:hidden">
               {[...Array(5)].map((_, i) => (
                 <Star
                   key={i}
@@ -49,7 +49,7 @@ export default function Heropage() {
             </div>
             <p className="mt-4 text-3xl font-bold">
               2k+ <span className="text-muted-foreground">Good Reviews</span>
-            </p> */}
+            </p>
           </div>
         </div>
       </div>
