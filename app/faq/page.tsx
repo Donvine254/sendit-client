9567cd3ebbf1f3087cdbd62502ff9a6e64866be0
Donvine <donvinemugendi@gmail.com>
2024-12-28 04:33:17 +0300

    add FAQ page with metadata for SEO optimization

---
+++ app/faq/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+type Props = {};
+export const metadata: Metadata = {
+  title: "Sendit Courier- Frequently Asked Questions ",
+  description:
+    "Sendit Courier provides courier delivery services that enables customers to send parcels from the comfort of their homes.",
+};
+export default function page({}: Props) {
+  return <div>page</div>;
+}
