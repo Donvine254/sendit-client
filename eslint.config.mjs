13eb850794223c94458fa2596321434eebc8d5a9
Donvine <donvinemugendi@gmail.com>
2024-12-29 15:26:53 +0300

    chore: update eslint configuration to use defineConfig and improve TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint-define-config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,10 +10,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = {
-  extends: [...compat.extends("next/core-web-vitals", "next/typescript")],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "error",
-  },
-};
-export default eslintConfig;
+export default defineConfig({
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [...compat.extends("next/core-web-vitals", "next/typescript")],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "error",
+      },
+    },
+  ],
+});
