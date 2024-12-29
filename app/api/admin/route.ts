c4a6b5c9a06b27cea900940a62b2b953f7236143
Donvine <donvinemugendi@gmail.com>
2024-12-29 13:57:37 +0300

    feat: improve error handling and validation in admin API route

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -14,17 +14,33 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { isAuthenticated } = getKindeServerSession();
-  if (!isAuthenticated) {
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-  }
-  const { id } = await req.json();
-  if (id) {
-    const params = {
-      id: id,
-    };
+  try {
+    const { id } = await req.json();
+    if (!id) {
+      return NextResponse.json(
+        { error: "User ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const { isAuthenticated } = getKindeServerSession();
+    if (!isAuthenticated) {
+      return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+    }
+
     init();
-    const userData = await Users.getUserData(params);
+    const userData = await Users.getUserData({ id });
+
+    if (!userData) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
     return NextResponse.json(userData);
-  } else return NextResponse.json({ error: "User not found" }, { status: 404 });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
