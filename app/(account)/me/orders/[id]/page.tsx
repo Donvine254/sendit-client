09cef3abb04e403fbc52cf3bc3c0c19a314d5680
Donvine <donvinemugendi@gmail.com>
2024-12-30 12:36:23 +0300

    feat: enable dynamic rendering for order page by updating static parameter generation

--- app/(account)/me/orders/[id]/page.tsx
+++ app/(account)/me/orders/[id]/page.tsx
@@ -4,30 +4,12 @@ import Orderform from "./order-form";
 import StatusBadge from "@/components/ui/status-badge";
 import Progress from "./order-progress";
 
-export async function generateStaticParams() {
-  try {
-    const orders = await prisma.parcel.findMany({
-      select: {
-        id: true,
-      },
-    });
-    const idArray = orders.map((order) => ({ id: order.id }));
-    console.log(idArray);
-    return idArray;
-  } catch (error) {
-    console.error("Error fetching order data:", error);
-    return [];
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
 type Props = {
   params: Promise<{
     id: string;
   }>;
 };
-
+export const dynamic = "force-dynamic";
 export default async function OrderPage({ params }: Props) {
   const { id } = await params;
   if (!id) {
