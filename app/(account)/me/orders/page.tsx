72c66043a79ee08554bd643127b00f629f1dcd27
Donvine <donvinemugendi@gmail.com>
2024-12-30 14:09:04 +0300

    feat: rename orders component to orders-table and update dynamic rendering logic

--- app/(account)/me/orders/page.tsx
+++ app/(account)/me/orders/page.tsx
@@ -1,17 +1,22 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { sessionUser } from "@/types";
 import { getUserOrders } from "@/lib/actions";
-import DataTable from "./orders";
+import DataTable from "./orders-table";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Sendit Courier- My Account | Orders",
   description:
     "Sendit Courier provides courier delivery services that enables customers to send parcels from the comfort of their homes.",
 };
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const { getUser } = getKindeServerSession();
+  let orders;
   const user = (await getUser()) as sessionUser;
-  const orders = await getUserOrders(user.id);
+  if (user) {
+    orders = await getUserOrders(user?.id);
+  }
+
   return (
     <section>
       <DataTable data={orders} />
