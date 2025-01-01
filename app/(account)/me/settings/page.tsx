45153e514d099f4a7d994aaf63b4c2d387abc141
Donvine <donvinemugendi@gmail.com>
2025-01-01 18:57:56 +0300

    feat: add "My Invoices" link to the navbar and create settings page

---
+++ app/(account)/me/settings/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Sendit Courier- My Account | Settings",
+  description:
+    "Sendit Courier provides courier delivery services that enables customers to send parcels from the comfort of their homes.",
+};
+export const dynamic = "force-dynamic";
+
+export default function Settings() {
+  return <div>page</div>;
+}
