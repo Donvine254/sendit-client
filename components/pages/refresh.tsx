6e105af238c3a398e4d7bd0d521f3b22e9a5196c
Donvine <donvinemugendi@gmail.com>
2025-01-04 22:46:00 +0300

    feat: enhance refresh component with syncing state and loading animation

--- components/pages/refresh.tsx
+++ components/pages/refresh.tsx
@@ -7,12 +7,17 @@ let intervalRef: NodeJS.Timeout;
 export default function Refresh({ tag }: { tag: string }) {
   // functions to allow users to sync data and invalidate cache
   const [timeSinceLoad, setTimeSinceLoad] = useState("less than a minute ago");
+  const [isSyncing, setIsSyncing] = useState(false);
   const router = useRouter();
   async function syncData() {
-    await revalidateCache(tag);
-    clearInterval(intervalRef);
-    setTimeSinceLoad("less than a minute ago");
-    router.refresh();
+    setIsSyncing(true);
+    setTimeout(async () => {
+      await revalidateCache(tag);
+      clearInterval(intervalRef);
+      setTimeSinceLoad("less than a minute ago");
+      setIsSyncing(false);
+      router.refresh();
+    }, 100);
   }
   //keep track of the time since the last update
   useEffect(() => {
@@ -36,8 +41,16 @@ export default function Refresh({ tag }: { tag: string }) {
   return (
     <div className="flex items-center justify-start pt-2 gap-2 text-muted-foreground">
       <p className="text-sm xsm:text-xs">Last updated {timeSinceLoad}</p>
-      <button title="sync now" onClick={syncData}>
-        <RefreshCcw className="h-4 w-4 text-blue-500 cursor-pointer hover:animate-spin transition-all delay-150" />
+      <button
+        title="sync now"
+        onClick={syncData}
+        disabled={isSyncing}
+        className="disabled:cursor-not-allowed disabled:opacity-50">
+        <RefreshCcw
+          className={`h-4 w-4 text-blue-500 cursor-pointer transition-all delay-150 ${
+            isSyncing ? "animate-spin" : "hover:animate-spin "
+          } `}
+        />
       </button>
     </div>
   );
