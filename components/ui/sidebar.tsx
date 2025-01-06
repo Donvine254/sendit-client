f720f0f06e31201cf657f170b24ccc7f78e0decd
Donvine <donvinemugendi@gmail.com>
2025-01-06 04:02:16 +0300

    feat: update sidebar component with new state management functions and add eslint dependency

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -27,8 +27,10 @@ const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 type SidebarContext = {
   state: "expanded" | "collapsed";
   open: boolean;
+  // eslint-disable-next-line no-unused-vars
   setOpen: (open: boolean) => void;
   openMobile: boolean;
+  // eslint-disable-next-line no-unused-vars
   setOpenMobile: (open: boolean) => void;
   isMobile: boolean;
   toggleSidebar: () => void;
@@ -50,6 +52,7 @@ const SidebarProvider = React.forwardRef<
   React.ComponentProps<"div"> & {
     defaultOpen?: boolean;
     open?: boolean;
+    // eslint-disable-next-line no-unused-vars
     onOpenChange?: (open: boolean) => void;
   }
 >(
@@ -73,6 +76,7 @@ const SidebarProvider = React.forwardRef<
     const [_open, _setOpen] = React.useState(defaultOpen);
     const open = openProp ?? _open;
     const setOpen = React.useCallback(
+      // eslint-disable-next-line no-unused-vars
       (value: boolean | ((value: boolean) => boolean)) => {
         const openState = typeof value === "function" ? value(open) : value;
         if (setOpenProp) {
