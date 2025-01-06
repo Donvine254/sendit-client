265015569765cdf1477ff23ae7aca6e9da35cc26
Donvine <donvinemugendi@gmail.com>
2025-01-06 16:02:04 +0300

    feat: add dynamic greeting based on time of day in header component

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -4,6 +4,22 @@ import { SidebarHeader, SidebarTrigger } from "../ui/sidebar";
 import { Separator } from "../ui/separator";
 
 export default function Header({ user }: { user: sessionUser }) {
+  function greetUser() {
+    const data: [number, number, string][] = [
+      [0, 4, "Good Night"],
+      [5, 11, "Good Morning"],
+      [12, 17, "Good Afternoon"],
+      [18, 24, "Good Night"],
+    ];
+    const hr = new Date().getHours();
+
+    for (let i = 0; i < data.length; i++) {
+      if (hr >= data[i][0] && hr <= data[i][1]) {
+        return data[i][2];
+      }
+    }
+  }
+
   return (
     <SidebarHeader className="fixed top-0 h-20 z-10 bg-white w-full  border-b">
       <div className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
@@ -12,7 +28,7 @@ export default function Header({ user }: { user: sessionUser }) {
           <Separator orientation="vertical" className="mr-2 h-4" />
           <p className="tracking-tight leading-tight inline-flex flex-col py-2 ">
             <span className="font-semibold leading-tight capitalize">
-              Good morning {user?.given_name}
+              {greetUser()} {user?.given_name}
             </span>
             <span className="text-muted-foreground text-xs sm:text-sm">
               {new Date().toLocaleDateString(undefined, {
