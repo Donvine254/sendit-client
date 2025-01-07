5ea8b7857aab1fc8b292f7fc4a9778e5e5e14f35
Donvine <donvinemugendi@gmail.com>
2025-01-07 03:19:56 +0300

    update cmdk peer dependencies

--- app/(admin)/admin/dashboard/deliveries/data-table.tsx
+++ app/(admin)/admin/dashboard/deliveries/data-table.tsx
@@ -359,7 +359,7 @@ export default function ParcelDataTable({ data }: { data: Parcel[] }) {
         <div className="relative flex-1 xsm:w-full">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-white h-4 w-4" />
           <Input
-            placeholder="Search by description..."
+            placeholder="Search all columns..."
             value={globalFilter ?? ""}
             onChange={(event) => setGlobalFilter(String(event.target.value))}
             className="flex-1 w-full  xsm:text-sm pl-8 dark:text-white"
