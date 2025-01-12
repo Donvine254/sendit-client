a24e1e139df7a76887104098a82fe7aba14becf7
Donvine <donvinemugendi@gmail.com>
2025-01-12 22:38:42 +0300

    feat: implement timezone selection with cookie storage and update theme selector order

--- app/(admin)/admin/dashboard/settings/timezone-selector.tsx
+++ app/(admin)/admin/dashboard/settings/timezone-selector.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import { getCookie, setCookie } from "@/lib/utils";
+import { ChangeEvent, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const TimezoneSelector = () => {
   const [timezones, setTimezones] = useState<string[]>([]);
+  const [selected, setSelected] = useState("");
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -11,8 +14,20 @@ const TimezoneSelector = () => {
     if (typeof Intl.supportedValuesOf === "function") {
       setTimezones(Intl.supportedValuesOf("timeZone"));
     }
+    // get the cookie
+    const storedTimezone = getCookie("timezone");
+    if (storedTimezone) {
+      setSelected(storedTimezone);
+    } else setSelected("Africa/Nairobi");
   }, []);
-
+  const handleTimezoneChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const newTimezone = event.target.value;
+    setSelected(newTimezone);
+    setCookie("timezone", newTimezone, 365);
+    toast.success("Timezone updated", {
+      position: "top-right",
+    });
+  };
   if (!mounted) {
     return (
       <select
@@ -27,11 +42,10 @@ const TimezoneSelector = () => {
 
   return (
     <select
-      defaultValue="Africa/Nairobi"
+      value={selected}
+      onChange={handleTimezoneChange}
       className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
-      <option value="" selected>
-        Select Timezone
-      </option>
+      <option value="">Select Timezone</option>
       {timezones.map((zone, index) => (
         <option key={index} value={zone}>
           {zone}
--- lib/utils.ts
+++ lib/utils.ts
@@ -86,3 +86,25 @@ export const exportData = (
     link.click();
   }
 };
+
+// Function to set a cookie
+export const setCookie = (cname: string, cvalue: string, exdays: number) => {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = `${cname}=${cvalue};${expires};path=/`;
+};
+
+// Function to get a cookie by name
+export const getCookie = (cname: string) => {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
